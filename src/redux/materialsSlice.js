d717981b8f65b0fd94c71621ddbc0ff4452a7a63
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-11-13 09:30:41 +0200

    Update materialsSlice.js

--- src/redux/materialsSlice.js
+++ src/redux/materialsSlice.js
@@ -1,50 +1,14 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-export const materialsApi = createApi({
-  reducerPath: 'materials',
-  baseQuery: fetchBaseQuery({
-    baseUrl: 'https://62584f320c918296a49543e7.mockapi.io',
-  }),
-  tagTypes: ['Material'],
-  endpoints: builder => ({
-    getMaterials: builder.query({
-      query: () => '/materials',
-      providesTags: ['Material'],
-    }),
-    getMaterialById: builder.query({
-      query: id => `/materials/${id}`,
-      providesTags: ['Material'],
-    }),
-    addMaterial: builder.mutation({
-      query: values => ({
-        url: '/materials',
-        method: 'POST',
-        body: values,
-      }),
-      invalidatesTags: ['Material'],
-    }),
-    deleteMaterial: builder.mutation({
-      query: id => ({
-        url: `/materials/${id}`,
-        method: 'DELETE',
-      }),
-      invalidatesTags: ['Material'],
-    }),
-    updateMaterial: builder.mutation({
-      query: fields => ({
-        url: `/materials/${fields.id}`,
-        method: 'PUT',
-        body: fields,
-      }),
-      invalidatesTags: ['Material'],
-    }),
+
+export const contactsApi = createApi({
+  reducerPath: 'contacts',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://6551cd285c69a77903291a91.mockapi.io' }),
+  endpoints: (builder) => ({
+    getContacts: builder.query({
+      query: () => `/contacts`,
+    }),      
   }),
 });
 
-export const {
-  useGetMaterialsQuery,
-  useAddMaterialMutation,
-  useDeleteMaterialMutation,
-  useGetMaterialByIdQuery,
-  useUpdateMaterialMutation,
-} = materialsApi;
+export const { useGetContactsQuerry } = contactsApi;
